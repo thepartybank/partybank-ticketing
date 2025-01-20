d39d941b77cabe8a91848cabea811f2cb30622dc
whitechapel007 <ebukareed@gmail.com>
2025-01-20 22:29:18 +0100

    chore:small fixes

--- src/components/home/HaveAnEvent.vue
+++ src/components/home/HaveAnEvent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col lg:flex-row px-3 gap-[26px] mt-16 lg:mt-[75px]">
+  <div class="flex flex-col md:flex-row px-3 gap-[26px] mt-16 lg:mt-[75px]">
     <div class="bg-primary-red text-white rounded-[20px] flex-1 lg:pl-14 pt-8 lg:pt-14 pb-6 px-4">
       <h2 class="text-6xl font-redHat font-medium">
         Have an <br class="hidden lg:block" />
@@ -14,13 +14,13 @@
       </p>
 
       <button
-        class="bg-white rounded-full text-primary-red py-3 w-full mt-10 lg:mt-[70px] lg:w-[236px]"
+        class="bg-white rounded-full text-primary-red py-3 w-full mt-10 lg:mt-[70px] sm:w-[236px]"
       >
         Create an event
       </button>
     </div>
     <div class="flex-1">
-      <img src="/images/img_5.webp" alt="" class="min-h-[350px] lg:w-[initial]" />
+      <img src="/images/human 1.png" alt="" class="h-full object-fill" />
     </div>
   </div>
 </template>
