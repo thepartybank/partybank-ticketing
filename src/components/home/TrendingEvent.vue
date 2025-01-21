012ef4887b32d2edcc82403eb599839668f8a159
whitechapel007 <ebukareed@gmail.com>
2025-01-21 11:58:36 +0100

    responsive fixes

--- src/components/home/TrendingEvent.vue
+++ src/components/home/TrendingEvent.vue
@@ -12,7 +12,8 @@
           '-translate-x-[-3%] md:-translate-x-[-3%]': currentIndex === 0,
           '-translate-x-[75%] md:-translate-x-[40%]': currentIndex === 1,
 
-          '-translate-x-[150%] md:-translate-x-[80%]': currentIndex === 2,
+          '-translate-x-[150%] sm:-translate-x-[136%] md:-translate-x-[80%] xl:-translate-x-[80%] lg:-translate-x-[105%]':
+            currentIndex === 2,
         }"
         ref="slider"
       >
@@ -20,7 +21,7 @@
         <div
           v-for="(item, index) in items"
           :key="index"
-          class="shrink-0 w-full max-w-[309px] md:max-w-[846px] sm:max-w-[586px] sm:max-h-[507px] max-h-[419px] md:max-h-[707px] relative"
+          class="shrink-0 w-full max-w-[309px] xl:max-w-[846px] sm:max-w-[506px] sm:max-h-[457px] max-h-[419px] xl:max-h-[707px] lg:max-h-[607px] lg:max-w-[706px] relative"
           :class="{
             'scale-100 z-10 h-[707px]': index === currentIndex,
             'scale-90 h-[607px]': index !== currentIndex,
@@ -44,16 +45,19 @@
 
       <!-- Navigation Buttons -->
 
-      <button class="text-black px-4 py-2 rounded absolute bottom-1 left-[12%]" @click="prevSlide">
+      <button class="text-black px-4 py-2 rounded absolute -bottom-2 left-[12%]" @click="prevSlide">
         PREV <img src="" alt="" />
       </button>
-      <button class="text-black px-4 py-2 rounded absolute bottom-1 right-[15%]" @click="nextSlide">
+      <button
+        class="text-black px-4 py-2 rounded absolute -bottom-2 right-[15%]"
+        @click="nextSlide"
+      >
         NEXT
       </button>
     </div>
   </section>
 
-  <div class="flex justify-center pb-10">
+  <div class="flex justify-center pb-10 mt-5">
     <button class="bg-primary-red rounded-full text-white h-10 md:h-14 px-9 md:px-16">
       See all events
     </button>
