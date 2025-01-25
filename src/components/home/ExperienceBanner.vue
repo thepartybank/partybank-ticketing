4a93b8af73d844d8309b892fab980b9db6dbbc24
whitechapel007 <ebukareed@gmail.com>
2025-01-25 14:35:00 +0100

    fixed

--- src/components/home/ExperienceBanner.vue
+++ src/components/home/ExperienceBanner.vue
@@ -1,17 +1,19 @@
 <template>
   <div
-    class="w-[95%] xl:max-w-[1300px] mx-auto h-[436px] lg:bg-[url('/images/Experienced.svg')] bg-[url('/images/Experienced-mobile.svg')] bg-no-repeat bg-contain mt-32 pl-5 pr-5 pt-6 md:pl-12 md:pt-12"
+    class="w-[95%] xl:max-w-[1300px] mx-auto h-[357px] sm:h-[436px] lg:bg-[url('/images/Experienced.svg')] bg-[url('/images/Experienced-mobile.svg')] bg-no-repeat bg-cover mt-32 pl-5 pr-5 pt-6 md:pl-12 lg:pt-12 md:pt-10 rounded-b-3xl rounded-tr-3xl flex flex-col md:pb-10 pb-8"
   >
-    <h2 class="text-white text-[40px] sm:text-[77px] sm:leading-[88px] leading-[50px] mb-3 ms:mb-7">
+    <h2 class="text-white text-[40px] md:text-[77px] md:leading-[88px] leading-[50px] mb-3 md:mb-7">
       Share your event <br />
       Experience
     </h2>
 
-    <p class="text-white text-xl mb-3 ms:mb-7 sm:w-[540px]">
+    <p class="text-white text-xl sm:w-[540px]">
       You will be directed to fill a form and upload a picture of your experience in an event
     </p>
 
-    <button class="bg-primary-red text-white rounded-full h-14 px-10 text-xl font-redHat">
+    <button
+      class="bg-primary-red text-white rounded-full h-14 px-5 text-lg font-redHat mt-auto md:w-[255px]"
+    >
       Share your experience
     </button>
   </div>
